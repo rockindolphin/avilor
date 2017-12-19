bdef0b4b04f2d05c3b08e255da788648433c247b
Unknown <rockindolphing@gmail.com>
2017-12-19 12:08:58 +0300

    chosen2

--- js/app.js
+++ js/app.js
@@ -67,7 +67,6 @@ $(document).ready(function() {
 		'.chosen-select-deselect'  : { allow_single_deselect: true },
 		'.chosen-select-no-single' : { disable_search_threshold: 10 },
 		'.chosen-select-no-results': { no_results_text: 'Ничего не найдено' },
-		'.chosen-select-rtl'       : { rtl: true },
 		'.chosen-select-width'     : { width: '95%' }
 	};
 	for (var selector in config) {
